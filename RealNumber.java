b66b8b52762b7672e7ff51a01282c859ce942307
Stacy <skim10@stuy.edu>
2020-11-23 00:05:34 -0500

    Added equals method

--- RealNumber.java
+++ RealNumber.java
@@ -12,4 +12,13 @@ public class RealNumber{
   public String toString(){
     return ""+getValue();
   }
+
+  public boolean equals(RealNumber other){
+    if (getValue() == 0 || other.getValue() == 0){
+      if (getValue() == 0 && other.getValue() == 0) return true;
+      else return false;
+    }
+    else return Math.abs(getValue() - other.getValue()) <= 1e-5 *
+                Math.abs(other.getValue());
+  }
 }
