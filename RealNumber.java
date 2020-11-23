a4a694a372885cf7f0fb57447ee13d05b8cca42f
Stacy <skim10@stuy.edu>
2020-11-23 00:10:43 -0500

    Added add and multiply methods

--- RealNumber.java
+++ RealNumber.java
@@ -21,4 +21,14 @@ public class RealNumber{
     else return Math.abs(getValue() - other.getValue()) <= 1e-5 *
                 Math.abs(other.getValue());
   }
+
+  public RealNumber add(RealNumber other){
+    RealNumber sum = new RealNumber(getValue() + other.getValue());
+    return sum;
+  }
+
+  public RealNumber multiply(RealNumber other){
+    RealNumber product = new RealNumber(getValue() * other.getValue());
+    return product;
+  }
 }
